19024523646af76fc8b3c807041390e5896b99c7
Mikhail <mikhail.sapuzhak@gmail.com>
2020-02-26 13:54:09 +0200

    add functions for adding new task

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const tasks = [
   {
     _id: '5d2ca9e2e03d40b326596aa7',
@@ -31,4 +33,88 @@ const tasks = [
   },
 ];
 
-(function(arrOfTasks) {})(tasks);
+(function(arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  //Elements UI
+  const listContainer = document.querySelector('.tasks-list-section .list-group');
+  const form = document.forms['addTask'];
+  const inputTitle = form.elements['title'];
+  const inputBody = form.elements['body'];
+
+  renderAllTasks(objOfTasks);
+  form.addEventListener('submit', onFormSubmit);
+
+  function renderAllTasks(taskList) {
+    if (!taskList) {
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+
+    Object.values(taskList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+
+    listContainer.appendChild(fragment);
+  }
+
+  function listItemTemplate({ _id, title, body} = {}) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete task';
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
+  }
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+  
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    if (!titleValue || !bodyValue) {
+      alert('Please, enter the text in the indicated fields');
+      return;
+    }
+
+    const task = createNewTask(titleValue, bodyValue);
+    const listItem = listItemTemplate(task);
+
+    listContainer.insertAdjacentElement('afterbegin', listItem);
+    form.reset();
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    };
+
+    objOfTasks[newTask._id] = newTask;
+
+    return { ...newTask };
+  }
+
+})(tasks);
